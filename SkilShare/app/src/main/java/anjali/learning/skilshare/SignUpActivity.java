576c538d6e4dd55fbf2e0af4c8758ecb2f5eda1c
Valani Anjali <anjalivalani728@gmail.com>
2025-07-12 13:37:44 +0530

    "Changed signup code"

--- SkilShare/app/src/main/java/anjali/learning/skilshare/SignUpActivity.java
+++ SkilShare/app/src/main/java/anjali/learning/skilshare/SignUpActivity.java
@@ -29,7 +29,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     DatabaseReference database;
-
+//comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
