fc831ebb9c8579ec6325619c59f3f20a2bf360aa
Anjali Valani <anjalivalani728@gmail.com>
2025-07-12 13:11:58 +0530

    Update MainActivity.java
    fixed navigation drawer

--- SkilShare/app/src/main/java/anjali/learning/skilshare/MainActivity.java
+++ SkilShare/app/src/main/java/anjali/learning/skilshare/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
 
-        // Toolbar
+        // Toolbar code
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
